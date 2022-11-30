ca4173c75eab55f0b58dd3d3ba6363006f26509e
conwayn816 <77367414+conwayn816@users.noreply.github.com>
2022-11-30 14:58:01 -0500

    Add files via upload

---
+++ HumanPlayer.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+//Human player derives from player
+
+public class HumanPlayer extends Player
+{
+
+    public HumanPlayer(int turn)
+    {
+        super(turn);
+    }
+
+    public void TakeTurn(Player othPlayer)
+    {
+        int choice;
+        Scanner in = new Scanner(System.in);
+        do{
+            System.out.print("Player " + GetTurn()+ " please enter a move (1-9): ");
+            choice = in.nextInt();
+            if (choice < 1 || choice > 9)
+                System.out.println("Invalid choice. Please enter 1-9");
+            else if (!valid(choice))
+            {
+                System.out.println("Invalid choice. Please enter a different choice");   
+            }
+            else{
+                switch (choice)
+                {
+                case 1: board[0][0] = letter;
+                        break;
+                case 2: board[0][1] = letter;
+                        break;  
+                case 3: board[0][2] = letter;
+                        break; 
+                case 4: board[1][0] = letter;
+                        break; 
+                case 5: board[1][1] = letter;
+                        break; 
+                case 6: board[1][2] = letter;
+                        break;   
+                case 7: board[2][0] = letter;
+                        break;   
+                case 8: board[2][1] = letter;
+                        break;  
+                case 9: board[2][2] = letter;
+                        break;        
+                }
+                break;
+            }
+
+        }while ((choice < 1 || choice > 9) || (!valid(choice)));
+    }
+   
+
+    
+}
---
+++ TicTacToe.java
@@ -0,0 +1,123 @@
+//By Nathan Conway for COP3252 Fall 2022
+//Contains main for the game loop
+
+import java.util.Scanner;
+import java.lang.Integer;
+import java.lang.String;
+
+
+public class TicTacToe
+{
+    
+    public static void main(String[] args)
+    {
+        
+        boolean win = false;
+        int winner = 0;
+        //create array of the players
+        Player[] players = new Player[2];
+        
+        //if there are arguments then check the arguments
+        if (args.length > 0)
+        {
+            //if only argument is -c create two computer players
+            if (args.length == 1)
+            {
+                players[0] = new ComputerPlayer(1);
+                players[1] = new ComputerPlayer(2);
+            }
+
+            //if two args check both
+            else if (args.length == 2)
+            {
+                //make computer player first
+                if (Integer.parseInt(args[1]) == 1)
+                {
+                    players[0] = new ComputerPlayer(1);
+                    players[1] = new HumanPlayer(2);
+                }
+                //make human player first
+                else if (Integer.parseInt(args[1]) == 2)
+                {
+                    players[0] = new HumanPlayer(1);
+                    players[1] = new ComputerPlayer(2);
+                }
+                //print usage message because args are incorrect
+                else{
+                    System.out.println("Usage:  java TicTacToe [-c [1|2]]");        
+                }
+
+            }
+            //if arg length is wrong then print usage method
+            else {
+                System.out.println("Usage:  java TicTacToe [-c [1|2]]");
+            }
+
+
+        }
+        //no args then create two human players
+        else
+        {
+            players[0] = new HumanPlayer(1);
+            players[1] = new HumanPlayer(2);
+        }
+        int i = 0;
+        int count = 0;
+        do{
+            players[i].DrawGame();
+            DrawExampleBoard();
+
+            if (i == 0)
+            {
+                players[i].TakeTurn(players[i+1]);
+
+                win = players[i].CheckWinner();
+                if ( win == true)
+                {
+                    winner = players[i].GetTurn();
+                }
+                i++;
+            }
+            else if (i == 1)
+            {
+                players[i].TakeTurn(players[i-1]);
+                
+                win = players[i].CheckWinner();
+                if ( win == true)
+                {
+                    winner = players[i].GetTurn();
+                }
+                i--;
+            }
+            count++;
+
+        } while ((win == false) && (count < 9));
+
+        players[0].DrawGame();
+
+        if( win == true)
+            System.out.println("Player "+winner+" wins!");
+        else
+            System.out.println("It is a draw!");
+
+
+
+
+    }
+
+
+    //helper function for drawing he example board
+    private static void DrawExampleBoard()
+    {
+        System.out.println("Positions:\n");
+        for (int i = 0; i < 9 ; i = i+3)
+        {
+            System.out.println(" "+(i+1)+" | "+(i+2)+" | "+(i+3));
+            if (i < 6)
+                System.out.println("-----------");
+        }
+
+
+
+    }
+}
